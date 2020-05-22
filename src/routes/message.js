5af5bd8b9bb191a1857e950f3ef0c7caf51becc6
Robin Wieruch <wrobin@gmx.net>
2020-05-22 10:05:53 +0200

    mongoose-rest

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,36 +1,36 @@
-import { v4 as uuidv4 } from 'uuid';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
+router.get('/', async (req, res) => {
+  const messages = await req.context.models.Message.find();
+  return res.send(messages);
 });
 
-router.get('/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
+router.get('/:messageId', async (req, res) => {
+  const message = await req.context.models.Message.findById(
+    req.params.messageId,
+  );
+  return res.send(message);
 });
 
-router.post('/', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
+router.post('/', async (req, res) => {
+  const message = await req.context.models.Message.create({
     text: req.body.text,
-    userId: req.context.me.id,
-  };
-
-  req.context.models.messages[id] = message;
+    user: req.context.me.id,
+  });
 
   return res.send(message);
 });
 
-router.delete('/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
+router.delete('/:messageId', async (req, res) => {
+  const message = await req.context.models.Message.findById(
+    req.params.messageId,
+  );
 
-  req.context.models.messages = otherMessages;
+  if (message) {
+    await message.remove();
+  }
 
   return res.send(message);
 });
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
+router.get('/', async (req, res) => {
+  const users = await req.context.models.User.find();
+  return res.send(users);
 });
 
-router.get('/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
+router.get('/:userId', async (req, res) => {
+  const user = await req.context.models.User.findById(
+    req.params.userId,
+  );
+  return res.send(user);
 });
 
 export default router;
