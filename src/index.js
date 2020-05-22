badb83b70d2582f50bdf45630e24d2ea5a6a2a6b
Robin Wieruch <wrobin@gmx.net>
2020-05-22 10:03:20 +0200

    seed

--- src/index.js
+++ src/index.js
@@ -44,9 +44,43 @@ connectDb().then(async () => {
       models.User.deleteMany({}),
       models.Message.deleteMany({}),
     ]);
+
+    createUsersWithMessages();
   }
 
   app.listen(process.env.PORT, () =>
     console.log(`Example app listening on port ${process.env.PORT}!`),
   );
 });
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'rwieruch',
+  });
+
+  const user2 = new models.User({
+    username: 'ddavids',
+  });
+
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id,
+  });
+
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id,
+  });
+
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id,
+  });
+
+  await message1.save();
+  await message2.save();
+  await message3.save();
+
+  await user1.save();
+  await user2.save();
+};
